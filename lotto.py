70f533d8770d7ee0fbf02689d02f3037ee5a07c3
MateuszPiskorski <mpiskorski51@gmail.com>
2022-05-28 14:05:10 +0200

    lotto game created and tested

---
+++ lotto.py
@@ -0,0 +1,28 @@
+from random import sample
+
+
+def lotto():
+    player_numbers = []
+    for _ in range(6):
+        player_numbers.append(input("Provide number from 1 to 49 (numbers cannot be repeated): "))
+
+    try:
+        player_numbers = list(map(int, player_numbers))
+    except ValueError:
+        return print("You can provide numbers only"), lotto()
+
+    for num in player_numbers:
+        if num in range(1, 50):
+            if player_numbers.count(num) > 1:
+                return print("Repeated number:", num), lotto()
+            pass
+        else:
+            print("Number not in range:", num), lotto()
+    print("Your choices:", sorted(player_numbers))
+    guessed_numbers = sample(range(1, 50), 6)
+    print("Today winners:", sorted(guessed_numbers))
+    player_scores = [num for num in player_numbers if num in guessed_numbers]
+    print(f"You scored {len(player_scores)} number(s): {player_scores}")
+
+
+lotto()
